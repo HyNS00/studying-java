dfbe9bb9aabb40c63197990e313e54a98c26ceb7
Hyso <hsk7953@gmail.com>
2023-11-03 22:35:44 +0900

    클래스 기초 활용1

---
+++ src/sec05/chap02/ex01/Slime.java
@@ -0,0 +1,13 @@
+package sec05.chap02.ex01;
+
+public class Slime {
+    double hp = 50;
+    int attack = 8;
+    double defense = 0.2;
+
+    void attack (Slime enemy){
+        // Slime enemy라는 것은 Slime이라는 객체 이름명은 enemy
+        // 즉 다른 슬라임 객체를 인자로 받는다.
+        enemy.hp -= attack * (1 - enemy.defense);
+    }
+}
