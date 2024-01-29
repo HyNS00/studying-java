03f4d87eb1d1ea2968a858db9b9c692c55c987ef
Hyso <hsk7953@gmail.com>
2024-01-29 17:33:36 +0900

    clone

---
+++ src/sec07/chap04/ShallowCopied.java
@@ -0,0 +1,23 @@
+package sec07.chap04;
+
+public class ShallowCopied implements Cloneable{
+    String title;
+    int no;
+
+    int[] numbers;
+    Click click;
+    Click[] clicks;
+
+    public ShallowCopied(String title, int no, int[] numbers, Click click, Click[] clicks) {
+        this.title = title;
+        this.no = no;
+        this.numbers = numbers;
+        this.click = click;
+        this.clicks = clicks;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
+}
