5ffcb91d6ffa5e5165965baab29fd9710d2b9cce
Hyso <hsk7953@gmail.com>
2024-04-09 16:43:44 +0900

    :sparkles: 의존성 주입 따라해보기 기본 구성들

---
+++ src/sec13/chap04/ex03/Character.java
@@ -0,0 +1,11 @@
+package sec13.chap04.ex03;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Character {
+}
