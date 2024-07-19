ca32a911829ef6e5ea362dac2c2a001668fea9f8
Hyso <hsk7953@gmail.com>
2024-07-19 22:45:53 +0900

    containsAll test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("끼끼끼");
+        List<String> list1 = Arrays.asList("a", "b", "c");
+        List<String> list2 = Arrays.asList("a", "b", "c");
+        System.out.println(list2.containsAll(list1));
+        System.out.println(list1.containsAll(list2));
     }
 }
